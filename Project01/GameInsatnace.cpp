14feaf75d7b68f6379b91e8761287f65fd464556
xodbs8825 <97510008+xodbs8825@users.noreply.github.com>
2022-09-04 17:26:02 +0900

    Create GameInsatnace.cpp

---
+++ Project01/GameInsatnace.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	FString CharacterDataPath = TEXT("/Game/GameData/MyCharacterData.MyCharacterData");
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_MYCHARACTER(*CharacterDataPath);
+
+	if (DT_MYCHARACTER.Succeeded())
+	{
+		MyCharacterTable = DT_MYCHARACTER.Object;
+		ensure(MyCharacterTable->GetRowMap().Num() > 0);
+	}
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Warning, TEXT("UMyGameInstance::Init()"));
+}
+
+FMyCharacterData* UMyGameInstance::GetMyCharacterData(int32 Level)
+{
+	return MyCharacterTable->FindRow<FMyCharacterData>(*FString::FromInt(Level), TEXT(""));
+}
